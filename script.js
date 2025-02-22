7705cbfe9861b1e2af153df64330be10f7bdfa74
DexonOff <omirbekabylay@gmail.com>
2025-02-22 12:57:35 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,66 @@
-document.documentElement.requestFullscreen(); // Фуллскрин
+document.addEventListener("DOMContentLoaded", function () {
+    // 🌕 Вход в полноэкранный режим
+    function goFullScreen() {
+        if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) {
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullscreen) {
+            document.documentElement.webkitRequestFullscreen();
+        } else if (document.documentElement.msRequestFullscreen) {
+            document.documentElement.msRequestFullscreen();
+        }
+    }
 
-let errorSound = new Audio("error.mp3");
-errorSound.play(); // Включаем звук ошибки
+    // 🔊 Воспроизведение звука ошибки
+    function playErrorSound() {
+        let audio = new Audio("error.mp3");
+        audio.volume = 1.0;
+        audio.play().catch(() => {
+            console.log("Автовоспроизведение заблокировано");
+        });
+    }
 
-window.addEventListener("beforeunload", (event) => {
-    event.preventDefault();
-    event.returnValue = "";
+    // 🖱️ Курсор начинает дрожать
+    function shakeCursor() {
+        setInterval(() => {
+            window.moveBy(Math.random() * 10 - 5, Math.random() * 10 - 5);
+        }, 50);
+    }
+
+    // 🚫 Предупреждение при закрытии
+    window.onbeforeunload = function () {
+        return "Ты уверен, что хочешь уйти?";
+    };
+
+    // 🔵 Фейковый BSOD через 5 секунд
+    function fakeBSOD() {
+        setTimeout(() => {
+            let bsod = document.createElement("div");
+            bsod.style.position = "fixed";
+            bsod.style.top = "0";
+            bsod.style.left = "0";
+            bsod.style.width = "100vw";
+            bsod.style.height = "100vh";
+            bsod.style.backgroundColor = "#0000AA";
+            bsod.style.color = "white";
+            bsod.style.fontSize = "20px";
+            bsod.style.fontFamily = "monospace";
+            bsod.style.textAlign = "center";
+            bsod.style.paddingTop = "20vh";
+            bsod.innerHTML = "A problem has been detected and Windows has been shut down to prevent damage to your computer.<br><br>ERROR: SYSTEM FAILURE<br><br>Press any key to restart...";
+            document.body.appendChild(bsod);
+
+            // Блокируем клавиши (Esc не работает в браузере)
+            document.onkeydown = function () {
+                location.reload();
+            };
+        }, 5000);
+    }
+
+    // Запуск функций
+    goFullScreen();
+    playErrorSound();
+    shakeCursor();
+    fakeBSOD();
 });
-
-setInterval(() => { // Дрожащий курсор
-    window.moveBy(Math.random() * 10 - 5, Math.random() * 10 - 5);
-}, 50);
-
-setTimeout(() => { // Фейковый BSOD через 5 секунд
-    document.body.innerHTML = "";
-    document.body.style.backgroundColor = "blue";
-    let bsod = document.createElement("div");
-    bsod.innerHTML = "<h1 style='color:white; font-size:40px;'>A problem has been detected and Windows has been shut down to prevent damage.</h1>";
-    document.body.appendChild(bsod);
-}, 5000);
